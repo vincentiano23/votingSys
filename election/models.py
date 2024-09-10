100c048e1645c9d2463c61be1357bc278d3eb1be
vincentiano23 <vincentmuthiani15@gmail.com>
2024-09-10 12:41:42 +0300

    implement voter id validation and prevent multiple votes

--- election/models.py
+++ election/models.py
@@ -13,6 +13,14 @@ class Vote(models.Model):
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     voter_name = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f'{self.voter_name} voted for {self.candidate.name}'
+
+class Voter(models.Model):
+    voter_name = models.CharField(max_length=120, default="default_voter_name")
+    voter_id = models.CharField(max_length=8, unique=True,default="default_voter_id" )
+
+    def __str__(self):
+        return f"{self.voter_name} ({self.voter_id})"
+
+  
--- election/views.py
+++ election/views.py
@@ -1,29 +1,37 @@
 from django.shortcuts import render, redirect
-from .models import Candidate, Vote
+from .models import Candidate, Vote, Voter
 from django.http import HttpResponseForbidden
+from django.contrib import messages
 
 def candidate_list(request):
     candidates = Candidate.objects.all()
-    
+    voter_has_voted = False
+
     if request.method == 'POST':
         voter_name = request.POST.get('voter_name')
+        voter_id = request.POST.get('voter_id')
         candidate_id = request.POST.get('candidate_id')
         
-        if candidate_id:
-            candidate = Candidate.objects.get(id=candidate_id)
-            # Check if the voter has already voted
-            if Vote.objects.filter(voter_name=voter_name).exists():
-                return HttpResponseForbidden("You have already voted!")
-            else:
-                Vote.objects.create(candidate=candidate, voter_name=voter_name)
-                candidate.votes += 1
-                candidate.save()
-                return redirect('candidate_list')
-    
-    return render(request, 'election/candidate_list.html', {
+        voter = Voter.objects.filter(voter_id=voter_id).first()
+        if voter:
+            voter_has_voted = True
+            messages.error(request, f"voter with id {voter_id} has already voted!")
+        else:
+            selected_candidate = Candidate.objects.get(id=candidate_id)
+            selected_candidate.votes += 1
+            selected_candidate.save()
+
+            new_voter = Voter(voter_name= voter_name, voter_id = voter_id)
+            new_voter.save()
+
+            messages.success(request, f"Thank you, {voter_name}. Your vote has been recorded.")
+            return redirect('candidate_list')
+        
+    context = {
         'candidates': candidates,
-        'voter_has_voted': Vote.objects.filter(voter_name=request.POST.get('voter_name')).exists()
-    })
+        'voter_has_voted': voter_has_voted,
+    }
+    return render(request, 'election/candidate_list.html', context)
 
 # Define the `vote` view if needed
 def vote(request, candidate_id):
